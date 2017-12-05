185e3dfa64ec27179ca5e00e384b17698055a08f
casperlamboo <casperlamboo@gmail.com>
2017-12-05 19:53:26 +0100

    update title

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,7 @@ module.exports = {
 
 	plugins: [
     new HtmlWebpackPlugin({
-      title: 'Toon Shader',
+      title: 'Fish Eye',
       template: require('html-webpack-template'),
       inject: false,
       appMountId: 'app'
